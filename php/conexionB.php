c26219aaf08b8ff9e9216847c172af19705fb415
José Nunez Vivar <josehj2011@gmail.com>
2025-06-13 21:20:48 -0600

    Registross54

--- php/conexionB.php
+++ php/conexionB.php
@@ -1,14 +1,18 @@
 <?php
-session_start(); // Iniciar la sesión
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 $host = 'mysql.hostinger.com';
 $user = 'u648222299_hector';
 $password = 'Proyectou2025';
 $dbname = 'u648222299_base_hector';
 
-// Crear conexión
-$pdo = new mysqli($host, $user, $password, $dbname);
+// Crear conexión como variable global
+$GLOBALS['conn'] = new mysqli($host, $user, $password, $dbname);
 
 // Verificar conexión
-if ($pdo->connect_error) {
-    die("Conexión fallida: " . $pdo->connect_error);
+if ($GLOBALS['conn']->connect_error) {
+    die("Conexión fallida: " . $GLOBALS['conn']->connect_error);
 }
+?>
